b198bfee61d26a747b025902cdd4f8cee961322f
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-02-01 13:47:47 -0300

    getting login confirmation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <Header/>
+  <Header v-on:loggedState="setLoggedState"/>
+  <h1>Logged: {{loggedState}}</h1>
 </template>
 
 <script>
@@ -7,8 +8,18 @@ import Header from './components/Header.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      loggedState: false
+    }
+  },
   components: {
     Header
+  },
+  methods: {
+    setLoggedState(response){
+      this.loggedState = response.loggedState
+    }
   }
 }
 </script>
@@ -27,5 +38,10 @@ body {
 a {
     text-decoration: none;
     color: unset;
+}
+
+h1 {
+  color: white;
+  font-size: 50px;
 }
 </style>
