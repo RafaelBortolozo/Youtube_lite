1f1bebf8572e4bf96a8a50720a0407f6ea87b067
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-02-01 13:50:27 -0300

    exporting login status

--- src/components/LoginGoogle.vue
+++ src/components/LoginGoogle.vue
@@ -5,49 +5,59 @@
 </template>
 
 <script>
-import firebaseConfig from '../scripts/firebaseConfig'
-import { initializeApp } from 'firebase/app'
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
+import firebaseConfig from "../scripts/firebaseConfig";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 export default {
   name: "LoginGoogle",
   data() {
-    return { 
-        googleText: "Fazer login" 
-    }
+    return {
+      googleText: "Fazer login",
+      loggedState: false,
+    };
   },
   methods: {
-      googleSignIn: function() {
-          try {
-            initializeApp(firebaseConfig)
-            const provider = new GoogleAuthProvider()
-            const auth = getAuth()
-            signInWithPopup(auth, provider).then((result) => {
-                //const credential = GoogleAuthProvider.credentialFromResult(result)
-                //const token = credential.accessToken
-                const user = result.user
-                this.googleText = `Bem-vindo ${user.displayName}`
-                console.log(user)
-                
-            }).catch((error) => {
-                console.log(error)
-          })}catch(err) {
-            console.log(err)
-          }
+    googleSignIn: function () {
+      try {
+        initializeApp(firebaseConfig);
+        const provider = new GoogleAuthProvider();
+        const auth = getAuth();
+        signInWithPopup(auth, provider)
+          .then((result) => {
+            this.googleText = `Bem-vindo ${result.user.displayName}`;
+            this.loggedState = true
+            this.$emit("loggedState", {
+              loggedState: this.loggedState
+            });
+          })
+          .catch(() => {
+            this.googleText = "Login not worked, try again";
+            this.loggedState = false
+            this.$emit("loggedState", {
+              loggedState: this.loggedState,
+            });
+          });
+      } catch {
+        this.googleText = "Login not worked, try again";
+        this.loggedState = false
+        this.$emit("loggedState", {
+          loggedState: this.loggedState,
+        });
       }
-  }
+    },
+  },
 };
 </script>
 
 <style>
-  .login {
-    background-color: red;
-    border-radius: 50px;
-  }
-  button {
-    border: none;
-    padding: 7px;
-    border-radius: 50px;
-    cursor: pointer;
-  }
+.login {
+  border-radius: 50px;
+}
+button {
+  border: none;
+  padding: 7px;
+  border-radius: 50px;
+  cursor: pointer;
+}
 </style>
