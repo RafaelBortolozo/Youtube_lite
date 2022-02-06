564f60322b8670eb39efd8baf3e4654d6b45f6ea
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-02-06 12:05:22 -0300

    feat: request and listing of videos is working

--- src/components/SearchVideos.vue
+++ src/components/SearchVideos.vue
@@ -1,31 +1,58 @@
 <template>
-  <button v-on:click="updateList()">{{buttonText}}</button>
+    <button v-on:click="searchVideos">Atualizar lista</button>
+    <div class="videos">
+        <ItemVideo v-for="(video, index) of videos" :video="video" :key="index"/>
+    </div>
 </template>
 
 <script>
-//const axios = require('axios')
+import axios from "axios";
+import firebaseConfig from "../scripts/firebaseConfig.js";
+import ItemVideo from "./ItemVideo.vue";
 
 export default {
-    name: "SubsVideos",
-    data() {
-        return {
-            buttonText: "Atualizar lista"
-        }
-    },
-    methods: {
-        updateList() {
-            // let channels = 
-            // axios.get()
-        }
+  name: "SubsVideos",
+  data() {
+    return {
+      buttonText: "Atualizar lista",
+      videos: [],
+    };
+  },
+  components: {
+    ItemVideo,
+  },
+  props: {
+    title: {
+        type: String,
+        required: true
     }
-}
+  },
+  methods: {
+    async searchVideos() {
+      this.videos = []
+      const part = "snippet";
+      const API_KEY = firebaseConfig.apiKey;
+      const maxResults = 25;
+      const url = `https://www.googleapis.com/youtube/v3/search?part=${part}&key=${API_KEY}&maxResults=${maxResults}&q=${this.title}`;
+      if (this.title) {
+        axios({
+          method: "get",
+          url: url,
+        }).then((result) => {
+          this.videos = result.data.items
+          console.log(this.videos)
+        });
+      }
+    }
+  },
+};
 </script>
 
 <style>
-    button {
-        border: none;
-        padding: 7px;
-        border-radius: 50px;
-        cursor: pointer;
-    }
+button {
+  border: none;
+  padding: 7px;
+  border-radius: 50px;
+  cursor: pointer;
+}
 </style>
