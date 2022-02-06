32bdc9ddb2e30adff19cec1dfcb7d2129e930213
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-02-06 09:53:25 -0300

    feat: working video search

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,8 +8,8 @@
           </a>
         </div>
         <div class="searchBar">
-          <input type="text" placeholder="Search">
-          <a href="#"><img src="../assets/search_32.png"></a>
+          <input v-model="inputTextSearch" type="text" placeholder="Search">
+          <a href="#" v-on:click="searchVideos()"><img src="../assets/search_32.png"></a>
         </div>
         <LoginGoogle v-on:loginResult="setLoginResult"/>
       </div>
@@ -18,22 +18,47 @@
 
 <script>
 import LoginGoogle from "./LoginGoogle.vue";
+import axios from "axios";
+import firebaseConfig from "../scripts/firebaseConfig.js";
 
 export default {
   name: "Header",
+  data() {
+    return {
+      inputTextSearch: "Manual do mundo"
+    };
+  },
   components: {
     LoginGoogle,
   },
   methods: {
-    setLoginResult(response){
+    setLoginResult(response) {
       this.$emit("loginResult", {
         loggedState: response.loggedState,
-        loginResult: response.loginResult
-      })
-    }
-  }
+        loginResult: response.loginResult,
+      });
+    },
+
+    async searchVideos() {
+      const part = "snippet";
+      const API_KEY = firebaseConfig.apiKey;
+      const maxResults = 25;
+      const url = `https://www.googleapis.com/youtube/v3/search?part=${part}&key=${API_KEY}&maxResults=${maxResults}&q=${this.inputTextSearch}`;
+      if (this.inputTextSearch) {
+        axios({
+          method: "get",
+          url: url,
+        }).then((result) => {
+          this.$emit("resultSearchBar", {
+            resultSearchBar: result.data.items
+          })
+          console.log(this.videos);
+        });
+      }
+    },
+  },
 };
-</script>
+</script> 
 
 <style scoped>
 header {
@@ -67,7 +92,7 @@ header .youtube h1 {
   font-weight: 600;
   font-size: 20px;
   font-family: Arial, Helvetica, sans-serif;
-  transform: scale(1,1.8);
+  transform: scale(1, 1.8);
 }
 
 header .youtube img {
@@ -83,7 +108,7 @@ header .searchBar input {
   width: 75%;
   background-color: rgb(24, 24, 24);
   border: none;
-  color:white;
+  color: white;
   border-radius: 5px 0 0 5px;
   padding: 0 15px;
 }
