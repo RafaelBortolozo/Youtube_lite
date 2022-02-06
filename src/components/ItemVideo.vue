a5e3017825ca4283861bcf3721f8ccfaaad54d25
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-02-06 12:02:07 -0300

    feat: thumbnail display worked

---
+++ src/components/ItemVideo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="video">
+        <img :src="this.video.snippet.thumbnails.medium.url">
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "SubsVideos",
+  props: {
+    video: {
+        type: Object
+    }
+  }
+};
+</script>
+
+<style>
+    /* .video {
+        
+    } */
+</style>
