48bebfa057a71fa85d4f66c282fa5d37fc30a194
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-02-11 22:42:33 -0300

    feat: trying to use vuex

--- src/vuex.js
+++ src/vuex.js
@@ -1,28 +1,18 @@
-import { createApp } from 'vue'
 import { createStore } from 'vuex'
-
-Vue.use(Vuex)
 
 const store = createStore({
     state() {
         return {
-            loggedState: false,
-            searchState: false,
-            inputTextSearch: '',
-            unloggedMessage: 'O YouTube Lite precisa se conectar com a conta do Google para exibir videos da aba "Inscrições"',
-            loginResult: {}
+            unloggedMessage: 'O YouTube Lite precisa se conectar com a conta do Google para exibir videos da aba "Inscrições"'
         }
-    },
-    mutations: {
-        setLoginResult(state, response) {
-            state.loggedState = response.loggedState;
-            state.loginResult = response.loginResult;
-        },
-        setSearchVideos(response) {
-            this.searchState = true;
-            this.inputTextSearch = response.inputTextSearch;
-        },
+        // loggedState: false,
+        // searchState: false,
+        // inputTextSearch: '',
+        // loginResult: {}
     }
 })
 
-export default { store }
+export default store
+// const app = createApp({})
+// app.use(store)
+// app.mount('#app')
