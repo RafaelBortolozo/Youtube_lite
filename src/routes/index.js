ab1838483dee1c099e8b0b90ef8d5e347ecaf23d
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-02-11 22:41:36 -0300

    feat: added main route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,22 @@
-import VueRouter from 'vue-router'
-import Player from '../components/Player.vue'
-import SubsVideos from '../components/SubsVideos.vue'
-import SearchVideos from '../components/SearchVideos.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Player from '../views/Player'
+import SubsVideos from '../views/SubsVideos'
+import SearchVideos from '../views/SearchVideos'
+import Unlogged from '../views/Unlogged'
 
 const routes = [
-    { path: '/player', component: Player },
-    { path: '/subs', component: SubsVideos },
-    { path: '/search', component: SearchVideos }
+    { path: '/player', component: Player, name: 'player' },
+    { path: '/subs', component: SubsVideos, name: 'subs' },
+    { path: '/search', component: SearchVideos, name: 'search' },
+    { path: '/', component: Unlogged, name: 'main' }
 ]
 
-const router = VueRouter.createRouter({
-  history: VueRouter.createWebHashHistory(),
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
 export default router
+// const app = Vue.createApp({})
+// app.use(router)
+// app.mount('#app')
